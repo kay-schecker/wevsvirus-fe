16b79a8a334c5519f2321bc413b495633302c5d1
Kay Schecker <kay.schecker.ext@kiongroup.com>
2020-03-21 13:37:35 +0100

    add user id

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {generate} from 'short-uuid';
 
 @Component({
   selector: 'app-signup',
@@ -7,60 +8,6 @@ import {Component} from '@angular/core';
 })
 export class SignupPage {
 
-  public isSuccess: boolean;
-  public isError: boolean;
-  public isWarning: boolean;
-  public canProceed: boolean;
-  public pages: Array<{ title: string, thumb: string, description: string, link: any }>;
-
-  public   slideOpts = {
-    initialSlide: 1,
-    speed: 400
-  };
-
-  public item = {}
-
-  constructor() {
-    this.pages = [
-      {
-        title: 'About',
-        thumb: './assets/images/about.png',
-        description: 'Who we are. What we do. Why we’re here',
-        link: '/about'
-      },
-      {
-        title: 'Contact Us',
-        thumb: './assets/images/contact.png',
-        description: 'Drop us a line and get in touch we’d love to hear from you!',
-        link: '/contact'
-      },
-      {
-        title: 'Technologies',
-        thumb: './assets/images/tech.png',
-        description: 'Third party resources used by this app',
-        link: '/technology'
-      },
-      {
-        title: 'Tweets',
-        thumb: 'assets/images/twitter.png',
-        description: 'The latest news updates from our Twitter account',
-        link: 'TweetsPage'
-      }
-    ];
-  } // End of constructor
-
-  edit(page) {
-    console.log(`I could do a lot more than just print out this message for the ${page} page to the browser console`);
-  }
-
-
-  share(page) {
-    console.log(`I could do a lot more than just print out this message for the ${page} page to the browser console`);
-  }
-
-
-  delete(page) {
-    console.log(`Yep, I’m an under-performer for the ${page} page too :(`);
-  }
+  public readonly userId = generate()
 
 }
