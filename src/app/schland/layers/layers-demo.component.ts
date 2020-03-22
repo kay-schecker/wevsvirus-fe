4bdddbc305d7b343d896a370d83cd624186fc608
Kay Schecker <kay.schecker.ext@kiongroup.com>
2020-03-22 20:25:52 +0100

    fixes

--- src/app/schland/layers/layers-demo.component.ts
+++ src/app/schland/layers/layers-demo.component.ts
@@ -18,7 +18,7 @@ export class LeafletLayersDemoComponent {
     questionId: string;
 
     private readonly world = this.http.get('./assets/world3.json').toPromise();
-    private readonly data = this.http.get(`http://${window.location.hostname}:8080/heat?responseDate=${new Date().toISOString().substring(0, 10)}`).toPromise();
+    private readonly data = this.http.get(`http://${window.location.hostname}:8080/heat?responseDate=${new Date(new Date().getTime() - (3600 * 24 * 1000)).toISOString().substring(0, 10)}`).toPromise();
 
     constructor(
         private readonly http: HttpClient,
@@ -31,82 +31,78 @@ export class LeafletLayersDemoComponent {
     }[]> = Promise.all([this.world, this.data]).then(([mapWorld, data]) => {
 
 
-        return Object.entries(data).map(([questionId, d]) => {
-            console.log(d.map(([plz, value]) => [String(plz).slice(0, 2), value]));
+        return Object.entries(data).map(([questionId, d]) => ({
+            questionId,
+            chartOptions: ({
+                chart: {map: mapWorld},
+                title: {text: ''},
 
-            return {
-                questionId,
-                chartOptions: ({
-                    chart: {map: mapWorld},
-                    title: {text: ''},
-
-                    mapNavigation: {
-                        enabled: false,
-                        buttonOptions: {
-                            verticalAlign: 'bottom',
-                        },
+                mapNavigation: {
+                    enabled: false,
+                    buttonOptions: {
+                        verticalAlign: 'bottom',
                     },
+                },
 
-                    colorAxis: {
-                        dataClasses: [{
-                            from: 0,
-                            to: 1,
-                            color: '#FFFFFF',
-                        }, {
-                            from: 1,
-                            to: 20,
-                            color: '#BCFFF7',
-                        }, {
-                            from: 20,
-                            to: 40,
-                            color: '#91E5DB',
-                        }, {
-                            from: 40,
-                            to: 60,
-                            color: '#00BCA6',
-                        }, {
-                            from: 60,
-                            to: 80,
-                            color: '#00675B',
-                        }, {
-                            from: 80,
-                            to: 100,
-                            color: '#002C27',
-                        }],
-                        min: 1,
-                        max: 100,
-                        type: 'linear',
-                        minColor: '#FFFFFF',
-                        maxColor: '#000000',
-                    },
-
-                    tooltip: {
-                        headerFormat: '',
-                        pointFormat: '<strong>PLZ {point.properties.plz}XXX:</strong> {point.value}%',
-                    },
-
-                    legend: {
-                        enabled: false,
-                    },
-
-                    series: [{
-                        data: d.map(([plz, value]) => [String(plz).slice(0, 2), Math.ceil(value)]),
-                        keys: ['plz', 'value'],
-                        name: '',
-                        joinBy: 'plz',
-                        states: {
-                            hover: {
-                                color: '#a4edba',
-                                // format: 'LOREM {point.properties.plz}: {value}'
-                            },
-                        },
-                        dataLabels: {
-                            enabled: false,
-                        },
+                colorAxis: {
+                    dataClasses: [{
+                        from: 0,
+                        to: 1,
+                        color: '#FFFFFF',
+                    }, {
+                        from: 1,
+                        to: 20,
+                        color: '#BCFFF7',
+                    }, {
+                        from: 20,
+                        to: 40,
+                        color: '#91E5DB',
+                    }, {
+                        from: 40,
+                        to: 60,
+                        color: '#00BCA6',
+                    }, {
+                        from: 60,
+                        to: 80,
+                        color: '#00675B',
+                    }, {
+                        from: 80,
+                        to: 100,
+                        color: '#002C27',
                     }],
-                }),
-            };
-        });
+                    min: 1,
+                    max: 100,
+                    type: 'linear',
+                    minColor: '#FFFFFF',
+                    maxColor: '#000000',
+                },
+
+                tooltip: {
+                    headerFormat: '',
+                    pointFormat: '<strong>PLZ {point.properties.plz}XXX:</strong> {point.value}%',
+                },
+
+                legend: {
+                    enabled: false,
+                },
+
+                series: [{
+                    data: d.map(([plz, value]) => [String(plz).slice(0, 2), Math.ceil(value)]),
+                    keys: ['plz', 'value'],
+                    name: '',
+                    joinBy: 'plz',
+                    states: {
+                        hover: {
+                            color: '#a4edba',
+                            // format: 'LOREM {point.properties.plz}: {value}'
+                        },
+                    },
+                    dataLabels: {
+                        enabled: false,
+                    },
+                }],
+            }),
+        }));
 
     });
 
