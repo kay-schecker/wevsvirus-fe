e66e755df51f11693338e744cdd6acf38eb87a7e
Kay Schecker <kay.schecker.ext@kiongroup.com>
2020-03-22 13:13:23 +0100

    fixed map

--- src/app/schland/schland.page.ts
+++ src/app/schland/schland.page.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 
 import 'leaflet/dist/leaflet.css';
-import {circle, latLng, polygon, tileLayer} from 'leaflet';
 
 @Component({
     selector: 'app-schland',
@@ -10,29 +9,16 @@ import {circle, latLng, polygon, tileLayer} from 'leaflet';
 })
 export class SchlandPage implements OnInit {
 
-    options = {
-        layers: [
-            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 18, attribution: '...'}),
-        ],
-        zoom: 3,
-        // center: latLng(46.879966, -121.726909),
-        center: latLng(51.9481, 10, 26517),
-    };
-    layersControl = {
-        baseLayers: {
-            'Open Street Map': tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' }),
-            'Open Cycle Map': tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
-        },
-        overlays: {
-            'Big Circle': circle([ 46.95, -122 ], { radius: 5000 }),
-            'Big Square': polygon([[ 46.8, -121.55 ], [ 46.9, -121.55 ], [ 46.9, -121.7 ], [ 46.8, -121.7 ]])
-        }
-    };
+    showDemo = false;
 
-    constructor() {
-    }
 
     ngOnInit() {
+
+        // Primarily for debugging
+        setTimeout(() => {
+            this.showDemo = true;
+        }, 200);
+
     }
 
 }
